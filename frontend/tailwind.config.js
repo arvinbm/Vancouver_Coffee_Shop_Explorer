3ba4ed223af76acec504a0248ad781eea7906633
Arvin Bayat Manesh <aba191@sfu.ca>
2026-06-03 17:59:16 -0700

    Apply warm colour scheme and Plus Jakarta Sans font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,12 +6,19 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Plus Jakarta Sans', 'sans-serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
